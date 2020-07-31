34b44fefd1b22e433c6c60d33ad6c16d9179ab24
Smokyn <smokyn_cosmin@yahoo.com>
2020-07-31 21:23:39 +0300

    exploring the major loops

--- loops/for-loop.js
+++ loops/for-loop.js
@@ -1,7 +1,40 @@
-var i = 10;
-
-for (var i = 0; i < 5; i++) {
+for (let i = 0; i < 5; i++) {
   console.log(i);
 }
 
-console.log(i);
+const users = {
+  name: 'Cosmin',
+  age: 31,
+  greet() {
+    console.log('Hello');
+  },
+};
+
+users.__proto__.test = 5;
+
+for (const key in users) {
+  if (users.hasOwnProperty(key)) {
+    console.log(key);
+    console.log(users[key]);
+  }
+}
+
+// for (const val of Object.values(users)) {
+//   console.log(val);
+// }
+
+const numbers = [1, 2, 3, 4, 5];
+
+// for (const num of numbers) {
+//   console.log(num);
+// }
+
+// for (let i = 0; i < numbers.length; i++) {
+//   console.log(i);
+//   console.log(numbers[i]);
+// }
+
+numbers.forEach(function (num, index) {
+  console.log(num);
+  console.log('Index ' + index);
+});
