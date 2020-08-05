663210a3bc0654fcc034d511da1d9a9cea6a793f
Smokyn <smokyn_cosmin@yahoo.com>
2020-08-05 18:24:35 +0300

    mastering closures

---
+++ closures/tricky-example.js
@@ -0,0 +1,12 @@
+for (let i = 0; i < 5; i++) {
+    // (function() {
+    //     var j = i;
+    //     setTimeout(() => {
+    // console.log(j)
+    // }, 500)
+    // }());
+    setTimeout(() => {
+        console.log(i)
+    }, 500)
+
+}
