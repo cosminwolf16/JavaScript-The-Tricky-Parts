e5c49c58d65eea146b3f4412309856e4156120b9
Smokyn <smokyn_cosmin@yahoo.com>
2020-08-05 22:13:55 +0300

    Promises behind the scenes look

--- asynchronous-code/promises.js
+++ asynchronous-code/promises.js
@@ -1,3 +1,13 @@
+const myPromise = new Promise(function(resolve,reject) {
+    setTimeout(function() {
+    resolve("It worked")
+    }, 2000);
+});
+
+myPromise.then(function(successString) {
+console.log(successString)
+})
+
 fetch("https://jsonplaceholder.typicode.com/todos/1", {})
 .then(function(response) {
 return response.json();
