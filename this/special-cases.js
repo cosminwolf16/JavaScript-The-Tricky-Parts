6a7a3f2b8e42e8bfd3cf7fb4f3bbc08d2656fecb
Smokyn <smokyn_cosmin@yahoo.com>
2020-08-06 16:34:07 +0300

    what influences this and why

--- this/special-cases.js
+++ this/special-cases.js
@@ -1,1 +1,28 @@
 // special case number 1
+
+class Person {
+    constructor() {
+        this.age = 31;
+    }
+    greet() {
+    console.log("My age: " + this.age);
+    }
+
+    greetWithDelay() {
+        setTimeout(function() {
+            console.log("My age: " + this.age)
+        }, 1500)
+
+    }
+}
+
+const cosmin = new Person();
+cosmin.greet();
+cosmin.greetWithDelay();
+
+const btn = document.querySelector("button");
+
+
+btn.addEventListener("click", function() {
+    console.log(this);
+})
